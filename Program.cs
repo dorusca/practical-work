b4287b8e20565d4897eb6d4ca3e2c4b11201679a
Dorina <acsinoi.dorina@yandex.ru>
2022-11-12 22:33:40 +0300

    example1

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт его квадрат
+
+Console.WriteLine("Введите число,квадрат которого надо найти ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine($"Квадрат {number} равен {result}");
+
+
+// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да 
+//a = 2, b = 10 -> нет 
+//a = 9, b = -3 -> да 
+//a = -3 b = 9 -> нет
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 * number2 == number1)
+{
+    Console.WriteLine($"число {number1} является квадратом {number2}");
+}
+else
+{
+    Console.WriteLine($"число {number1} не является квадратом {number2}");
+}
+
+
+
+Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+ 
+
+Console.WriteLine("Дорогой пользователь, введи N:");
+int num = Convert.ToInt32(Console.ReadLine ());
+
+int current = num * (-1);
+while (current <= num) 
+{
+    Console.Write (current + "  ");
+current++;
+}
+
+
+
+Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+if (number > 99 && number < 1000){
+    int current =number % 10 ;
+    Console.WriteLine ($"Последняя цифра числа {number} - {current}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное!");
+}
+*/
---
+++ practica/Program.cs
@@ -0,0 +1,65 @@
+﻿/* Задача 2: напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32 (Console.ReadLine ());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32 (Console.ReadLine ());
+if (num1 > num2 )
+{
+    Console.WriteLine($"число {num1} является больше {num2}");
+}
+else
+{
+    Console.WriteLine($"число {num2} является больше {num1}");
+
+
+}
+Задача 4: Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32 (Console.ReadLine ());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32 (Console.ReadLine ());
+
+Console.WriteLine("Введите третье число");
+int num3 = Convert.ToInt32 (Console.ReadLine ());
+int max = num1;
+
+if ( num1 > max) max = num1;
+if ( num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine($"число {max} является максимальным");
+
+
+
+
+Задача 6: Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли щно без остатка)
+
+
+Console.WriteLine("Введите  число");
+int num = Convert.ToInt32 (Console.ReadLine ());
+if ( num % 2 == 0)
+{
+    Console.WriteLine($"число {num} является четным");
+}
+else
+{
+    Console.WriteLine($"число {num} не является четным ");
+}
+
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+
+*/
+
+Console.WriteLine("Дорогой пользователь, введи N:");
+int num = Convert.ToInt32(Console.ReadLine ());
+
+int current = num / num ;
+while (current <= num) 
+{
+    Console.Write (current + " ");
+current++;
+}
